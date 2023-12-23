f05c0cf670d5c9378cf2d304be59ee4e4126b3cd
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2023-12-23 01:57:05 +0100

    Rework 4

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,6 +3,29 @@ import './Card.css';
 import data from './data/data.json';
 
 function Card({ filters, onFilterChange, selectedFilters, setSelectedFilters }) {
+	const [filteredJobs, setFilteredJobs] = useState(data);
+
+	useEffect(() => {
+		// Apply filtering logic when filters change
+		const newFilteredJobs = data.filter((job) => {
+			return filters.every((filter) => {
+				const filterString = String(filter); // Ensure filter is a string
+				if (filterString.startsWith('role-')) {
+					return job.role === filterString.replace('role-', '');
+				} else if (filterString.startsWith('level-')) {
+					return job.level === filterString.replace('level-', '');
+				} else if (filterString.startsWith('language-')) {
+					return job.languages.includes(filterString.replace('language-', ''));
+				} else if (filterString.startsWith('tool-')) {
+					return job.tools.includes(filterString.replace('tool-', ''));
+				}
+				return false;
+			});
+		});
+
+		setFilteredJobs(newFilteredJobs);
+	}, [filters]);
+
 	const handleFilterClick = (filterType, value) => {
 		const updatedFilters = [...selectedFilters];
 		const filterIdentifier = `${filterType}-${value}`;
@@ -33,8 +56,20 @@ function Card({ filters, onFilterChange, selectedFilters, setSelectedFilters }) 
 
 	return (
 		<>
-			<div className="right-card-container">
-				<div className="chips-test">{filters.map(({ type, values }) => renderChips(type, values))}</div>
+			<div className="card">
+				<div className="left-card-container"></div>
+				<div className="right-card-container">
+					<div className="chips-test">{filters.map(({ type, values }) => renderChips(type, values))}</div>
+				</div>
+			</div>
+
+			{/* Display the filtered jobs */}
+			<div className="filtered-jobs-container">
+				{filteredJobs.map((job) => (
+					<div key={job.id} className={`test-card ${job.featured ? 'featured' : ''}`}>
+						{/* Render job details here */}
+					</div>
+				))}
 			</div>
 		</>
 	);
