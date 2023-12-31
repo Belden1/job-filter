2d5d637c15623037b7a57e6f6562da2f32407803
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2023-12-31 03:11:17 +0100

    Rework 6 - updating abit

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,30 +6,10 @@ function Card({ filters, onFilterChange, selectedFilters, setSelectedFilters }) 
 	const [filteredJobs, setFilteredJobs] = useState(data);
 
 	useEffect(() => {
-		// console.log('Filters:', filters);
-		// console.log('Filtered Jobs:', filteredJobs);
-		// Apply filtering logic when filters change
-		const newFilteredJobs = data.filter((job) => {
-			return filters.every((filter) => {
-				const filterString = String(filter); // Ensure filter is a string
-				if (filterString.startsWith('role-')) {
-					return job.role === filterString.replace('role-', '');
-				} else if (filterString.startsWith('level-')) {
-					return job.level === filterString.replace('level-', '');
-				} else if (filterString.startsWith('language-')) {
-					return job.languages.includes(filterString.replace('language-', ''));
-				} else if (filterString.startsWith('tool-')) {
-					return job.tools.includes(filterString.replace('tool-', ''));
-				}
-				return false;
-			});
-		});
-
-		setFilteredJobs(newFilteredJobs);
+		setFilteredJobs(data);
 	}, [filters]);
 
 	const handleFilterClick = (filterType, value) => {
-		// console.log('Filter Click:', filterType, value);
 		const updatedFilters = [...selectedFilters];
 		const filterIdentifier = `${filterType}-${value}`;
 
@@ -47,12 +27,10 @@ function Card({ filters, onFilterChange, selectedFilters, setSelectedFilters }) 
 	};
 
 	const renderChips = (job, type, items) => {
-		console.log('Rendering Chips:', type, items);
-
 		return items.map((item, index) => (
 			<div
 				key={`${type}-${index}`}
-				className={`chip ${job && selectedFilters.includes(`${type}-${item}`) ? 'selected' : ''}`}
+				className={`chip${job && selectedFilters.includes(`${type}-${item}`) ? ' selected' : ''}`}
 				onClick={() => job && handleFilterClick(type, item)}
 			>
 				{item}
@@ -62,20 +40,17 @@ function Card({ filters, onFilterChange, selectedFilters, setSelectedFilters }) 
 
 	return (
 		<>
-			{data.map((job) => (
+			{filteredJobs.map((job) => (
 				<div key={job.id} className={`card${job.featured ? ' featured' : ''}`}>
 					<div className="left-card-container">
 						<img src={job.logo} alt={job.company} />
-
 						<div className="text-card-container">
 							<div className="text-card-header">
 								<p className="company">{job.company}</p>
 								{job.new && <p className="text-chip">New!</p>}
 								{job.featured && <p className="text-chip">Featured</p>}
 							</div>
-
 							<h2>{job.position}</h2>
-
 							<div className="text-card-footer">
 								<p className="footer-text">{`${job.postedAt} • ${job.contract} • ${job.location}`}</p>
 							</div>
